954ff7e892afa836cec1155bac8415edfef9ceb1
Vetlugins <vetlugins@gmail.com>
2018-07-25 15:42:15 +0300

    status 405

--- dist/jquery.vegasFormSender.js
+++ dist/jquery.vegasFormSender.js
@@ -84,6 +84,8 @@
 						msg = 'Not connect.\n Verify Network.';
 					} else if (jqXHR.status == 404) {
 						msg = 'Requested page not found. [404]';
+					}  else if (jqXHR.status == 405) {
+						msg = 'Method Not Allowed. [405]';
 					} else if (jqXHR.status == 500) {
 						msg = 'Internal Server Error [500].';
 					} else if (exception === 'parsererror') {
