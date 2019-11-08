f38acd4a6c38a5712284c4522564adcf5616ab15
Alexander Vetlugin <vetlugins@gmail.com>
2019-11-09 01:30:18 +0300

    v1.2

--- dist/jquery.vegasFormSender.js
+++ dist/jquery.vegasFormSender.js
@@ -8,109 +8,128 @@
  * Программа для сборки данных с формы и отправки их на сервер через ajax,
  * упрощая обработку и выдачу результата...
  *
- * Инструкции.
  * Инициализация: $('#какая-то форма').vegasFormSender();
- *
- * Параметры:
- * action: путь до обработчика, так же может быть указан непосредственно в форме.
- * method: метод отправки get или post, так же может быть указан непосредственно в форме.
- * fields: дополнительные параметры, указываются в ввиде объекта {ключ: значение},
- * либо этот объект возвращается через колбэк например:
- * fields: function() {
- * 		return {ключ: значение}
- * }
- * redirectError: перенаправление на страницу с фатальной ошибкой
- * redirectSuccess: перенаправление на страницу после успешного запроса
- *
- * Обработчики событий:
- * Плагин возвращает два события, об удачной отправке или о фатальной ошибке:
- * success: function (data) {
- * 		// ответ пришел от сервера
- * }
- * error: function (status) {
- * 		// ответ не пришел от сервера
- * }
  */
+
 (function( $ ) {
-
+	
 	$.fn.vegasFormSender = function(options) {
-
-		var options = $.extend({
+		
+		options = $.extend({
 			error: function() {},
 			success: function() {},
-			beforeSend: function() {}
+			beforeSend: function() {},
+			validate: function() {}
 		}, arguments[0] || {});
-
-		var form = this,
+		
+		let form = this,
 			action = options.action || form.attr('action') || location.href,
 			method = options.method || form.attr('method') || 'get',
 			fields = options.fields  ? options.fields : false,
 			redirectSuccess = options.redirectSuccess || form.data('redirect-success') || false,
 			redirectError = options.redirectError  || form.data('redirect-error') || false;
-
+		
 		if (fields && typeof fields == 'function') {
 			fields = options.fields();
 		}
-
+		
+		let valid = true;
+		
 		form.on('submit', function () {
-
-			var data = new FormData(this);
-
-			if (fields) {
-				for(var name in fields) {
-					data.append(name, fields[name]);
-				}
+			
+			let data = new FormData(this),
+				$required = $(this).find('[data-validate]');
+			
+			if ($required.length) {
+				let errors = required($required);
+				
+				options.validate.call(this, form, errors);
+				
+				if (errors.length) valid = false;
 			}
-
-			$.ajax({
-				url: action,
-				method: method,
-				data: data,
-				cache: false,
-				contentType: false,
-				processData: false,
-				beforeSend: function() {
-					options.beforeSend.call(this, form);
-				},
-				success: function (data) {
-					if (redirectSuccess) {
-						window.location.href = redirectSuccess;
+			
+			if (valid) {
+				if (fields) {
+					for(var name in fields) {
+						if (typeof fields[name] === 'object') {
+							for(let key in fields[name]) {
+								let arr = Object .keys (fields[name][key]) .map (i => fields[name][key] [i]);
+								data.append(name, arr);
+							}
+						} else {
+							data.append(name, fields[name]);
+						}
 					}
-					options.success.call(this, data, form);
-				},
-				error: function (jqXHR, exception) {
-					var status = '';
-
-					if (jqXHR.status === 0) {
-						status = 'Not connect.\n Verify Network.';
-					} else if (jqXHR.status == 404) {
-						status = 'Requested page not found. [404]';
-					}  else if (jqXHR.status == 405) {
-						status = 'Method Not Allowed. [405]';
-					} else if (jqXHR.status == 500) {
-						status = 'Internal Server Error [500].';
-					} else if (exception === 'parsererror') {
-						status = 'Requested JSON parse failed.';
-					} else if (exception === 'timeout') {
-						status = 'Time out error.';
-					} else if (exception === 'abort') {
-						status = 'Ajax request aborted.';
-					} else {
-						status = 'Uncaught Error.\n' + jqXHR.responseText;
-					}
-
-					if (redirectError) {
-						window.location.href = redirectError;
-					}
-
-					options.error.call(this, status, form);
 				}
-			});
-
+				
+				$.ajax({
+					url: action,
+					method: method,
+					data: data,
+					cache: false,
+					contentType: false,
+					processData: false,
+					beforeSend: function () {
+						options.beforeSend.call(this, form);
+					},
+					success: function (data) {
+						let obj = data;
+						
+						if (redirectSuccess) {
+							window.location.href = redirectSuccess;
+						}
+						
+						options.success.call(this, obj, form);
+					},
+					error: function (jqXHR, exception) {
+						let msg = '';
+						
+						if (jqXHR.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (jqXHR.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (jqXHR.status == 422) {
+							msg = 'Unprocessable Entity. [422]';
+						}  else if (jqXHR.status == 405) {
+							msg = 'Method Not Allowed. [405]';
+						} else if (jqXHR.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (exception === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (exception === 'timeout') {
+							msg = 'Time out error.';
+						} else if (exception === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + jqXHR.responseText;
+						}
+						
+						if (redirectError) {
+							window.location.href = redirectError;
+						}
+						
+						options.error.call(this, msg, form);
+					}
+				});
+			}
+			
 			return false;
 		});
-
+		
+		function required($elements) {
+			let errors = [];
+			
+			$elements.each(function () {
+				let $el = $(this);
+				
+				if ($el.val() === '') {
+					errors.push($el);
+				}
+			});
+			
+			return errors;
+		}
+		
 		return this;
-
 	};
 })(jQuery);
