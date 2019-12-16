ecd2eadbce0142ccf5fe3ce1adfd6f4523ea78e4
Alexander Vetlugin <vetlugins@gmail.com>
2019-12-16 09:39:33 +0300

    Update jquery.vegasFormSender.js

--- dist/js/jquery.vegasFormSender.js
+++ dist/js/jquery.vegasFormSender.js
@@ -23,129 +23,132 @@
 			validate: function() {}
 		}, arguments[0] || {});
 		
-		let form = this,
-			action = options.action || form.attr('action') || location.href,
-			method = options.method || form.attr('method') || 'get',
-			fields = options.fields  ? options.fields : false,
-			mask = form.find('input[data-mask]') || false,
-			redirectSuccess = options.redirectSuccess || form.data('redirect-success') || false,
-			redirectError = options.redirectError  || form.data('redirect-error') || false;
+		let $form = this;
 		
-		if (fields && typeof fields == 'function') {
-			fields = options.fields();
-		}
-		
-		
-		if (mask) {
-			mask.each(function () {
-				let type = $(this).data('mask'),
-					params = {};
-				
-				if (type === 'phone') {
-					params.mask = "8(999) 999-99-99";
-				}
-				
-				$(this).inputmask(params);
-			});
-		}
-		
-		let valid = true;
-		
-		form.on('submit', function () {
+		$form.each(function () {
+			let form = $(this),
+				action = options.action || form.attr('action') || location.href,
+				method = options.method || form.attr('method') || 'get',
+				fields = options.fields  ? options.fields : false,
+				mask = form.find('input[data-mask]') || false,
+				redirectSuccess = options.redirectSuccess || form.data('redirect-success') || false,
+				redirectError = options.redirectError  || form.data('redirect-error') || false;
 			
-			let data = new FormData(this),
-				$required = $(this).find('[data-validate]');
-			
-			if ($required.length) {
-				let errors = required($required);
-				
-				options.validate.call(this, form, errors);
-				
-				if (errors.length) valid = false;
+			if (fields && typeof fields == 'function') {
+				fields = options.fields();
 			}
 			
-			if (valid) {
-				if (fields) {
-					for(var name in fields) {
-						if (typeof fields[name] === 'object') {
-							for(let key in fields[name]) {
-								let arr = Object .keys (fields[name][key]) .map (i => fields[name][key] [i]);
-								data.append(name, arr);
-							}
-						} else {
-							data.append(name, fields[name]);
-						}
+			if (mask && typeof $.fn.inputmask !== 'undefined') {
+				mask.each(function () {
+					let type = $(this).data('mask'),
+						params = {};
+					
+					if (type === 'phone') {
+						params.mask = "8(999) 999-99-99";
 					}
-				}
-				
-				$.ajax({
-					url: action,
-					method: method,
-					data: data,
-					cache: false,
-					contentType: false,
-					processData: false,
-					beforeSend: function () {
-						options.beforeSend.call(this, form);
-					},
-					success: function (data) {
-						let obj = data;
-						
-						if (redirectSuccess) {
-							window.location.href = redirectSuccess;
-						}
-						
-						options.success.call(this, obj, form);
-					},
-					error: function (jqXHR, exception) {
-						let status = '',
-							msg = '';
-						
-						if (jqXHR.status === 0) {
-							msg = 'Not connect.\n Verify Network.';
-						} else if (jqXHR.status == 404) {
-							msg = 'Requested page not found. [404]';
-						} else if (jqXHR.status == 422) {
-							msg = 'Unprocessable Entity. [422]';
-						}  else if (jqXHR.status == 405) {
-							msg = 'Method Not Allowed. [405]';
-						} else if (jqXHR.status == 500) {
-							msg = 'Internal Server Error [500].';
-						} else if (exception === 'parsererror') {
-							msg = 'Requested JSON parse failed.';
-						} else if (exception === 'timeout') {
-							msg = 'Time out error.';
-						} else if (exception === 'abort') {
-							msg = 'Ajax request aborted.';
-						} else {
-							msg = 'Uncaught Error.\n' + jqXHR.responseText;
-						}
-						
-						if (redirectError) {
-							window.location.href = redirectError;
-						}
-						
-						options.error.call(this, msg, form);
-					}
+					
+					$(this).inputmask(params);
 				});
 			}
 			
-			return false;
-		});
-		
-		function required($elements) {
-			let errors = [];
+			let valid = true;
 			
-			$elements.each(function () {
-				let $el = $(this);
+			form.on('submit', function () {
 				
-				if ($el.val() === '') {
-					errors.push($el);
+				let data = new FormData(this),
+					$required = $(this).find('[data-validate]');
+				
+				if ($required.length) {
+					let errors = required($required);
+					
+					options.validate.call(this, form, errors);
+					
+					if (errors.length) valid = false;
 				}
+				
+				if (valid) {
+					if (fields) {
+						for(var name in fields) {
+							if (typeof fields[name] === 'object') {
+								for(let key in fields[name]) {
+									let arr = Object .keys (fields[name][key]) .map (i => fields[name][key] [i]);
+									data.append(name, arr);
+								}
+							} else {
+								data.append(name, fields[name]);
+							}
+						}
+					}
+					
+					$.ajax({
+						url: action,
+						method: method,
+						data: data,
+						cache: false,
+						contentType: false,
+						processData: false,
+						beforeSend: function () {
+							options.beforeSend.call(this, form);
+						},
+						success: function (data) {
+							let obj = data;
+							
+							if (redirectSuccess) {
+								window.location.href = redirectSuccess;
+							}
+							
+							options.success.call(this, obj, form);
+						},
+						error: function (jqXHR, exception) {
+							let status = '',
+								msg = '';
+							
+							if (jqXHR.status === 0) {
+								msg = 'Not connect.\n Verify Network.';
+							} else if (jqXHR.status == 404) {
+								msg = 'Requested page not found. [404]';
+							} else if (jqXHR.status == 422) {
+								msg = 'Unprocessable Entity. [422]';
+							}  else if (jqXHR.status == 405) {
+								msg = 'Method Not Allowed. [405]';
+							} else if (jqXHR.status == 500) {
+								msg = 'Internal Server Error [500].';
+							} else if (exception === 'parsererror') {
+								msg = 'Requested JSON parse failed.';
+							} else if (exception === 'timeout') {
+								msg = 'Time out error.';
+							} else if (exception === 'abort') {
+								msg = 'Ajax request aborted.';
+							} else {
+								msg = 'Uncaught Error.\n' + jqXHR.responseText;
+							}
+							
+							if (redirectError) {
+								window.location.href = redirectError;
+							}
+							
+							options.error.call(this, msg, form);
+						}
+					});
+				}
+				
+				return false;
 			});
 			
-			return errors;
-		}
+			function required($elements) {
+				let errors = [];
+				
+				$elements.each(function () {
+					let $el = $(this);
+					
+					if ($el.val() === '') {
+						errors.push($el);
+					}
+				});
+				
+				return errors;
+			}
+		});
 		
 		return this;
 	};
