27a0ade325710ea5eda77216ecc8a6bb2ba5188b
Alexander Vetlugin <vetlugins@gmail.com>
2020-08-07 22:06:34 +0300

    validate full

--- assets/js/validate.vegasFormSender.js
+++ assets/js/validate.vegasFormSender.js
@@ -1,25 +1,36 @@
 window.ValidateFormSender = {
-	error: 'error',
+	phrases: {
+		empty: 'Это обязательное поле',
+		email: 'Вы ввели не верный email адрес',
+		password: 'Введенные пароли не сопадают'
+	},
 	arrange: function (errors, form) {
-		form.find('.vg-form-sender__selector').removeClass(ValidateFormSender.error);
-		
+		ValidateFormSender.removeError(form.find('.vg-form-sender__selector'));
+
 		$.each(errors, function (i, arr) {
 			if (arr.length) {
 				$.each(arr, function (n) {
 					var $self = arr[n],
 						$selector = $self.closest('.vg-form-sender__selector');
 
-					$selector.addClass(ValidateFormSender.error);
+					$selector.addClass('error');
+					$selector.append('<p>' + ValidateFormSender.phrases[i] + '</p>');
+					ValidateFormSender.setValue($self, $selector)
 				});
 			}
 		});
 	},
 
-	setValue: function ($self) {
+	setValue: function ($self, $selector) {
 		$self.on('change', function () {
-			$(this).removeClass('error').next('p').hide();
-			
+			ValidateFormSender.removeError($selector);
+
 			return false;
 		});
+	},
+	
+	removeError: function($selector) {
+		$selector.removeClass('error');
+		$selector.find('p').remove();
 	}
 };
