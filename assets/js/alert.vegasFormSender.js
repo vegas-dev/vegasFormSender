459168dc95681a42bab84655d0678ca041e96efb
Alexander Vetlugin <vetlugins@gmail.com>
2020-08-08 11:00:43 +0300

    alert full

--- assets/js/alert.vegasFormSender.js
+++ assets/js/alert.vegasFormSender.js
@@ -4,7 +4,7 @@ window.AlertFormSender = {
 		success: '<svg viewbox="0 0 500 500" class="am_Success_Icon"><path class="am_SVG_circle" d="m443.0136,114.07007a236.95276,236.95276 0 0 1 44.1553,137.73747c0,129.97005 -106.94772,236.96443 -236.91777,236.96443s-236.91777,-106.94772 -236.91777,-236.91777s106.94772,-236.96443 236.91777,-236.96443a236.99941,236.99941 0 0 1 168.72548,70.59483"></path><polyline class="am_SVG_check" points="104.4892349243164,309.2001647949219 195.57406616210938,402.9600524902344 418.9292297363281,85.03718566894531 "></polyline></svg>',
 		cross: '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 224.512 224.512" style="enable-background:new 0 0 224.512 224.512;" xml:space="preserve"><g><polygon style="fill:#010002;" points="224.507,6.997 217.521,0 112.256,105.258 6.998,0 0.005,6.997 105.263,112.254 0.005,217.512 6.998,224.512 112.256,119.24 217.521,224.512 224.507,217.512 119.249,112.254 "/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>'
 	},
-	
+
 	modal: function (alert) {
 		var svg;
 		
@@ -31,7 +31,7 @@ window.AlertFormSender = {
 		
 		return false;
 	},
-	
+
 	block: function (alert, form) {
 		var svg;
 		
@@ -51,76 +51,73 @@ window.AlertFormSender = {
 		
 		form.after(block);
 	},
-	
+
 	Action: {
 		draw: function (form) {
 			var elCompare = form.data('compare') || 'modal';
-			
+
 			if (elCompare === 'modal') {
 				AlertFormSender.modal('success');
 				AlertFormSender.modal('error');
 			} else if(elCompare === 'block') {
 				$('body').find('.vg-form-sender__block').remove();
-				
 				AlertFormSender.block('success', form);
 				AlertFormSender.block('error', form);
 			}
 		},
-		
+
 		success: function (msg, form, params) {
 			this.draw(form);
-			
-			var $btn = form.find('[type=submit]'),
-				text = $btn.data('btn-text') || LangFormSender[params.lang].alert.btn_text,
-				elCompare = form.data('compare') || 'modal',
-				title = $btn.data('success-text') || LangFormSender[params.lang].alert.success_title;
-			
-			$btn.attr('disabled', false).find('span').text(text);
-
-			if (elCompare === 'modal') {
-				var $modal = $('#modal-success');
-
-				$('.modal').modal('hide');
-				AlertFormSender.text(msg, title, $modal);
-				$modal.modal('show');
-			} else if(elCompare === 'block') {
-				var $block = $('.vg-form-sender__block.success');
-				
-				$block.addClass('show');
-				AlertFormSender.text(msg, title, $block);
-			}
+			this.btn(form, false, params);
+			this.compare(msg, form, params, 'success');
 		},
+
 		error: function (msg, form, params) {
 			this.draw(form);
-			
-			var $btn = form.find('[type=submit]'),
-				text = $btn.data('btn-text') || LangFormSender[params.lang].alert.btn_text,
-				elCompare = form.data('compare') || 'modal',
-				title = $btn.data('error-text') || LangFormSender[params.lang].alert.error_title;
-			
-			$btn.attr('disabled', false).find('span').text(text);
-			
-			if (elCompare === 'modal') {
-				var $modal = $('#modal-error');
-				
-				$('.modal').modal('hide');
-				AlertFormSender.text(msg, title, $modal);
-				$modal.modal('show');
-			} else if(elCompare === 'block') {
-				var $block = $('.vg-form-sender__block.error');
-				
-				$block.addClass('show');
-				AlertFormSender.text(msg, title, $block);
-			}
+			this.btn(form, false, params);
+			this.compare(msg, form, params, 'error');
 		},
 
 		beforeSend: function (form, params) {
+			this.btn(form, true, params);
+		},
+
+		btn: function (form, disabled, params) {
+			var $btn = form.find('[type=submit]'), text;
+			
+			if (disabled) {
+				text = $btn.data('text-send') || LangFormSender[params.lang].alert.text_send
+			} else {
+				text = $btn.data('btn-text') || LangFormSender[params.lang].alert.btn_text
+			}
+			
+			$btn.attr('disabled', disabled).find('span').text(text);
+		},
+
+		compare: function (msg, form, params, alert) {
 			var $btn = form.find('[type=submit]'),
-				text = $btn.data('text-send') || LangFormSender[params.lang].alert.text_send;
-			$btn.attr('disabled', true).find('span').text(text);
+				elCompare = form.data('compare') || 'modal',
+				title;
+
+			if (alert === 'success') {
+				title = $btn.data('success-text') || LangFormSender[params.lang].alert.success_title;
+			} else {
+				title = $btn.data('error-text') || LangFormSender[params.lang].alert.error_title;
+			}
+
+			if (elCompare === 'modal') {
+				var $modal = $('#modal-' + alert);
+				$('.modal').modal('hide');
+				AlertFormSender.text(msg, title, $modal);
+				$modal.modal('show');
+			} else if(elCompare === 'block') {
+				var $block = $('.vg-form-sender__block.' + alert);
+				$block.addClass('show');
+				AlertFormSender.text(msg, title, $block);
+			}
 		}
 	},
-	
+
 	text: function (msg, title, $element) {
 		$element.find('.msg').html(msg);
 		$element.find('.title').html(title);
--- assets/js/lang.vegasFormSender.js
+++ assets/js/lang.vegasFormSender.js
@@ -10,7 +10,21 @@ window.LangFormSender = {
 		validate: {
 			empty: 'Это обязательное поле',
 			email: 'Вы ввели не верный email адрес',
-			password: 'Введенные пароли не сопадают'
+			password: 'Введенные пароли не совпадают'
+		}
+	},
+	en: {
+		alert: {
+			success_title: 'Success!',
+			success_msg: 'Your email has been successfully sent!',
+			error_title: 'Error!',
+			text_send: 'We send...',
+			btn_text: 'Send message',
+		},
+		validate: {
+			empty: 'This is a required field',
+			email: 'You entered an invalid email address',
+			password: 'The passwords entered do not match'
 		}
 	}
 };
