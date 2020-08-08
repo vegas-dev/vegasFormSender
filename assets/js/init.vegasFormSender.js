900a0704f87e800fed0df11dad315275f95a2eb1
Alexander Vetlugin <vetlugins@gmail.com>
2020-08-08 11:29:27 +0300

    auto init

--- assets/js/init.vegasFormSender.js
+++ assets/js/init.vegasFormSender.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
-	var $forms = $('[data-vgFormSender]');
-	
-	$forms.vegasFormSender({
-		beforeSend: function (form, params) {
-			AlertFormSender.Action.beforeSend(form, params);
-		},
-		success: function (data, form, params) {
-			AlertFormSender.Action.success(data.msg, form, params);
-		},
-		error: function (msg, form, params) {
-			AlertFormSender.Action.error(msg, form, params);
-		},
-		validate: function (form, params, errors) {
-			ValidateFormSender.init(errors, form, params);
+	var $forms = $('[data-vgFormSender]'),
+		init = typeof VEGASFORMSENDER_INIT !== 'undefined';
+
+	if (init) {
+		if (!VEGASFORMSENDER_INIT) {
+			init = false;
 		}
-	});
+	} else {
+		init = true;
+	}
+	
+	if (init) {
+		$forms.vegasFormSender({
+			beforeSend: function (form, params) {
+				AlertFormSender.Action.beforeSend(form, params);
+			},
+			success: function (data, form, params) {
+				AlertFormSender.Action.success(data.msg, form, params);
+			},
+			error: function (msg, form, params) {
+				AlertFormSender.Action.error(msg, form, params);
+			},
+			validate: function (form, params, errors) {
+				ValidateFormSender.init(errors, form, params);
+			}
+		});
+	}
 
 	$forms.each(function () {
 		var $_form = $(this);
