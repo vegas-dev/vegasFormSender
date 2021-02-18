adad49841c0f8547b19cd7138c6a0e1483329f59
Alexander Vetlugin <vetlugins@gmail.com>
2021-02-18 11:44:06 +0300

    2.0.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# VEGAS-FORM-SENDER 2.0.7 (FEB 18, 2021)
+* when closing modal in newer version bootstrap 5, use data-bootstrap-version="5" on the form
+
 # VEGAS-FORM-SENDER 2.0.0 (AUGUST 08, 2020)
 * Added simple form validation
 * Added automatic initialization
--- assets/js/alert.vegasFormSender.js
+++ assets/js/alert.vegasFormSender.js
@@ -4,20 +4,27 @@ window.AlertFormSender = {
 		success: '<svg viewbox="0 0 500 500" class="am_Success_Icon"><path class="am_SVG_circle" d="m443.0136,114.07007a236.95276,236.95276 0 0 1 44.1553,137.73747c0,129.97005 -106.94772,236.96443 -236.91777,236.96443s-236.91777,-106.94772 -236.91777,-236.91777s106.94772,-236.96443 236.91777,-236.96443a236.99941,236.99941 0 0 1 168.72548,70.59483"></path><polyline class="am_SVG_check" points="104.4892349243164,309.2001647949219 195.57406616210938,402.9600524902344 418.9292297363281,85.03718566894531 "></polyline></svg>',
 		cross: '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 224.512 224.512" style="enable-background:new 0 0 224.512 224.512;" xml:space="preserve"><g><polygon style="fill:#010002;" points="224.507,6.997 217.521,0 112.256,105.258 6.998,0 0.005,6.997 105.263,112.254 0.005,217.512 6.998,224.512 112.256,119.24 217.521,224.512 224.507,217.512 119.249,112.254 "/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>'
 	},
+	boot_version: 4,
 
 	modal: function (alert) {
-		let svg;
+		let svg, dismiss;
 		
 		if (alert === 'success') {
 			svg = AlertFormSender.Svg.success
 		} else {
 			svg = AlertFormSender.Svg.error
 		}
+
+		if (AlertFormSender.boot_version > 4) {
+			dismiss = 'data-bs-dismiss="modal"';
+		} else {
+			dismiss = 'data-dismiss="modal"';
+		}
 		
 		let modal = '<div class="modal fade vg-form-sender__modal" id="modal-'+ alert +'" tabindex="-1" role="dialog" aria-labelledby="modal-'+ alert +'" aria-hidden="true">' +
 			'    <div class="modal-dialog" role="document">' +
 			'        <div class="modal-content">' +
-			'            <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">' + AlertFormSender.Svg.cross + '</button>' +
+			'            <button type="button" class="modal-close" ' + dismiss + ' aria-label="Close">' + AlertFormSender.Svg.cross + '</button>' +
 			'            ' +
 			'            <div class="vg-form-sender__modal--alert vg-form-sender__modal--alert-'+ alert +'">' + svg +
 			'                <div class="title"></div>' +
@@ -55,6 +62,8 @@ window.AlertFormSender = {
 	Action: {
 		draw: function (form) {
 			let elCompare = form.data('compare') || 'modal';
+
+			AlertFormSender.boot_version = form.data('bootstrap-version') || 4;
 
 			if (elCompare === 'modal') {
 				AlertFormSender.modal('success');
